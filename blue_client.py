b8a601371a039d047cd3a518be7dfe2faca0a27d
Jonathan DeBarro <jdebarro@calpoly.edu>
2025-06-04 21:45:25 -0700

    testing

--- blue_client.py
+++ blue_client.py
@@ -1,22 +1,30 @@
+# import serial
+# import time
+
+# # For Linux: use '/dev/rfcomm0'
+# # For Windows: use 'COM5', 'COM6', etc.
+# port = 'COM7'  # or 'COM5'
+
+# try:
+#     ser = serial.Serial(port, baudrate=9600, timeout=1)
+#     time.sleep(1)  # Give it a second to settle
+
+#     # Send a command
+#     ser.write(b'ping\n')
+
+#     # Read the response
+#     response = ser.readline().decode().strip()
+#     print("Received:", response)
+
+#     ser.close()
+
+# except serial.SerialException as e:
+#     print("Could not open port:", e)
+
 import serial
-import time
+ser = serial.Serial('COM5', baudrate=9600)  # baudrate is ignored for BT SPP
+ser.write(b'Hello Pi\n')
+response = ser.readline()  # assuming the Pi echoes a line
+print("Got reply:", response.decode())
+ser.close()
 
-# For Linux: use '/dev/rfcomm0'
-# For Windows: use 'COM5', 'COM6', etc.
-port = 'COM7'  # or 'COM5'
-
-try:
-    ser = serial.Serial(port, baudrate=9600, timeout=1)
-    time.sleep(1)  # Give it a second to settle
-
-    # Send a command
-    ser.write(b'ping\n')
-
-    # Read the response
-    response = ser.readline().decode().strip()
-    print("Received:", response)
-
-    ser.close()
-
-except serial.SerialException as e:
-    print("Could not open port:", e)
